4d60e37c640f4fabe79993f298c24beb097c7ee7
Gene Phelps <gpphelpsii@gmail.com>
2021-06-23 17:38:18 -0400

    Edited SearchBooks.js to use the SAVE_BOOK mutation

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+
+// Imports the useMutation hook and the SAVE_BOOK mutation
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations'
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -52,6 +56,9 @@ const SearchBooks = () => {
     }
   };
 
+  // Uses the SAVE_BOOK mutation to save to book data to the ApolloServer
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
+
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
@@ -64,6 +71,7 @@ const SearchBooks = () => {
       return false;
     }
 
+    // Uses the saveBook mutation
     try {
       const response = await saveBook(bookToSave, token);
 
