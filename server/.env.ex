5aec4c7d7f5b602d123ce22c2c341a543d5c02d3
Gene Phelps <gpphelpsii@gmail.com>
2021-06-26 14:58:37 -0400

    Changed mutations.js, typeDefs.js, and Book.js so that books without descriptions can be saved by the user

---
+++ server/.env.ex
@@ -0,0 +1,2 @@
+MONGODB_URI = mongodb+srv://GPP:simplepassword@cluster0.zcj2w.mongodb.net/google-books?retryWrites=true&w=majority
+NODE_ENV = deployment
--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -32,7 +32,7 @@ const typeDefs = gql`
     type Mutation {
         addUser(username: String!, email: String!, password: String!): Auth
         login(email: String!, password: String!): Auth
-        saveBook(bookId: String!, authors: [String]!, description: String!, image: String, link: String, title: String!): User
+        saveBook(bookId: String!, authors: [String]!, description: String, image: String, link: String, title: String!): User
         removeBook(bookId: String!): User
     }
 `;
